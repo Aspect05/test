d639bdd7f86d810f08d6e0568fa67b549f79cd05
Aspect05 <83395973+Aspect05@users.noreply.github.com>
2021-05-01 20:28:17 +1400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.get('', (req, res) => {
+    res.sendFile('index')
+})
+app.listen(port, () => {
+    console.log(`App up at port ${port}`)
+})
