5cb7f3d4efe440d334c799cef5d1d9c134efb4d2
Aspect05 <83395973+Aspect05@users.noreply.github.com>
2021-05-01 20:37:38 +1400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 app.get('', (req, res) => {
-    res.sendFile('index')
+    res.send('index')
 })
 app.listen(port, () => {
     console.log(`App up at port ${port}`)
